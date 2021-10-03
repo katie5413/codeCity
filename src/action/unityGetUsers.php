c2e966fb35d36e8697a7efa4a15bdec721703003
13 <yishan13@25sprout.com>
2021-10-03 17:36:43 +0800

    fix: Unity 多送出學生照片

--- src/action/unityGetUsers.php
+++ src/action/unityGetUsers.php
@@ -2,13 +2,18 @@
 session_start();
 include('../../pdoInc.php');
 
-$sth = $dbh->prepare('SELECT name,id,classID FROM student');
-$sth->execute();
+if (isset($_SESSION["user"]['classID'])) {
 
-if ($sth->rowCount() > 0) {
-    while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {
-        echo
-        $row["id"] . "," . $row["name"] . "," . $row["classID"] . "\n";
+    $sth = $dbh->prepare('SELECT name,id,classID,img,img_name FROM student WHERE classID=?');
+    $sth->execute(array($_SESSION["user"]['classID']));
+
+    if ($sth->rowCount() > 0) {
+        while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {
+            echo
+            $row["id"] . "," . $row["name"] . "," . $row["classID"] . "," . $row["img"] . "," . $row["img_name"] . "\n";
+        }
+    } else {
+        echo "0 results";
     }
 } else {
     echo "0 results";
