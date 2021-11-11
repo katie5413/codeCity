f337285b4dbaae27252d833148f34314e45b4836
13 <yishan13@25sprout.com>
2021-11-11 15:06:29 +0800

    fix: Add coins data in unityGetUsers.

--- src/action/unityGetUsers.php
+++ src/action/unityGetUsers.php
@@ -4,13 +4,13 @@ include('../../pdoInc.php');
 
 if (isset($_SESSION["user"]['classID'])) {
 
-    $sth = $dbh->prepare('SELECT name,id,classID,img,img_name FROM student WHERE classID=?');
+    $sth = $dbh->prepare('SELECT name,id,classID,img,img_name,coins FROM student WHERE classID=?');
     $sth->execute(array($_SESSION["user"]['classID']));
 
     if ($sth->rowCount() > 0) {
         while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {
             echo
-            $row["id"] . "," . $row["name"] . "," . $row["classID"] . "," . $row["img"] . "," . $row["img_name"] . "\n";
+            $row["id"] . "," . $row["name"] . "," . $row["classID"] . "," . $row["img"] . "," . $row["img_name"] . ",". $row["coins"] . "\n";
         }
     } else {
         echo "0 results";
